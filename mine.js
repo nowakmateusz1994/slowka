5b3aa200827810bbcd56ea756260ebcdde24f070
Mateusz Nowak <39436410+nowakmateusz1994@users.noreply.github.com>
2019-04-07 18:27:01 +0200

    proste klasy
    Dodano proste klasy do gry w słówka,
    na koniec musze je spiąć w klasie game która będzie obsługiwać DOM

--- mine.js
+++ mine.js
@@ -1,23 +1,77 @@
 const letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż';
-const keyboard = document.querySelector('.keyboard')
+const keyboard = document.querySelector('.keyboard');
 
-const words = {
-    zwierze: ['lama', 'pies', 'kot'],
-    przedmiot: ['komputer', 'samochód', 'telefon'],
-    imie: ['Ewelina', 'Mateusz', 'Zuzanna'],
-    miasto: ['Konin', 'Poznań', 'Kraków']
+
+class Lifes {
+    constructor(lifes) {
+        this.lifes = lifes
+    }
+
+    checkHaveLife() {
+        if (this.lifes > 0) return true;
+        else return false;
+    }
+    changeNumberLifes(type = '-') {
+        if (type = '-') return lifes -= 1;
+    }
 }
 
-const categoryArr = [words.zwierze, words.przedmiot, words.imie, words.miasto];
+class DrawWord {
+    constructor() {
+        const words = ['Konin', 'poznań', 'warszawa', 'kraków', 'kielce', 'Jelenia Góra'];
+        let _result = this.drawResult();
+        this.getDrawResult = () => _result;
+    }
 
-        const createKeyboard = () => {
-            for (let i = 0; i < letters.length; i++) {
-                const el = document.createElement("div");
-                el.innerText = letters[i].toUpperCase();
-                el.classList.add("letter");
-                keyboard.appendChild(el);
+    drawResult() {
+        let word;
+        const index = Math.floor(Math.random() * this.words.length);
+        word = this.words[index];
+        return word;
+    }
+}
 
-            }
+class Result {
+    constructor(word) {
+        let wordArr = [...this.word];
+        let checkedLetter = 0;
+    }
+    static checkLetter(letter, wordArr) {
+        for (let i = 0; i > wordArr.length; i++) {
+            if (this.letter === this.wordArr[i][0]) {
+                this.wordArr[i][1] = true;
+                return true;
+            } else return false
         }
+    });
 
-        createKeyboard();
+
+static checkWord(wordArr) {
+    for (let i = 0; i > wordArr.length; i++) {
+        if (this.wordArr[i][1]) {
+            this.checkedLetter++;
+        }
+    }
+    if (this.checkedLetter === wordArr.length) return true;
+    else return false;
+}
+}
+
+
+
+
+
+
+const createKeyboard = () => {
+    for (let i = 0; i < letters.length; i++) {
+        const el = document.createElement("div");
+        el.innerText = letters[i].toUpperCase();
+        el.classList.add("letters");
+        el.classList.add("keys");
+        el.classList
+        keyboard.appendChild(el);
+
+    }
+}
+
+createKeyboard();
